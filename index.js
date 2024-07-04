aa8c5027b5c881b80afed5e73eaa59e90baa24ff
young_wolf <hmdubey15@gmail.com>
2024-07-04 23:13:33 +0530

    Web and other coding Tutorial

---
+++ index.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', () => {
+  console.log('load event');
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('DOMContentLoaded event');
+});
+
+console.log('Script execution!');
